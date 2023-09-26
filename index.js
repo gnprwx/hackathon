6f79f54c0c51c0abbc1c7eda03b5fe3e2a1314e8
Gregory Pappas <gnpappas@proton.me>
2023-09-26 08:42:56 -1000

    Have two objects

--- index.js
+++ index.js
@@ -39,29 +39,44 @@ const colors = [
 ];
 const rColors = colors[Math.floor(Math.random() * colors.length)];
 
-let firstInput = Math.floor(Math.random() * 6 + 1);
-let secondInput = Math.floor(Math.random() * 6 + 1);
-let thirdInput = Math.floor(Math.random() * 6 + 1);
+let width = Math.floor(Math.random() * 6 + 1);
+let height = Math.floor(Math.random() * 6 + 1);
+let depth = Math.floor(Math.random() * 6 + 1);
 
-const geometry = new THREE.BoxGeometry(firstInput, secondInput, thirdInput);
-const material = new THREE.MeshBasicMaterial({ color: rColors });
+const color = 0xFFFFFF;
+const intensity = 3;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
+
+const geometry = new THREE.BoxGeometry(width, height, depth);
+const geometry2 = new THREE.BoxGeometry(width, height, depth);
+const material = new THREE.MeshPhongMaterial({ color: rColors });
 const cube = new THREE.Mesh(geometry, material);
+const cube2 = new THREE.Mesh(geometry2, material);
 scene.add(cube);
+scene.add(cube2);
 
 camera.position.z = 10;
 
 const speed = document.querySelector("#randomSpeed");
+
 let rotateX = 0.01;
 let rotateY = 0.01;
+
 speed.addEventListener("click", () => {
     rotateX = Math.random() * 0.1;
     rotateY = Math.random() * 0.1;
 });
-
+  
 function animate() {
     requestAnimationFrame(animate);
     cube.rotation.x += rotateX;
     cube.rotation.y += rotateY;
+    cube.position.x = -3;
+    cube2.rotation.x += rotateX;
+    cube2.rotation.y += rotateY;
+    cube2.position.x = 3;
     renderer.render(scene, camera);
 }
 animate();
